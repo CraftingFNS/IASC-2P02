43bcb2f0051c5aef90166faa081247c38111a6e6
CraftingFNS <>
2025-01-26 18:55:05 -0500

    Prototype1 Edit

--- prototype1/script.js
+++ prototype1/script.js
@@ -8,7 +8,7 @@ const canvas = document.querySelector('.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('gray')
+scene.background = new THREE.Color('#6888BE')
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -18,7 +18,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 )
 scene.add(camera)
-camera.position.set(0, 0, 5)
+camera.position.set(0, 0, 7)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -35,7 +35,12 @@ const sphereGeometry = new THREE.SphereGeometry(1)
 const sphereMaterial = new THREE.MeshNormalMaterial(1)
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
+const torusKnotGeometry = new THREE.TorusKnotGeometry( 2.5, .2, 64, 16 ); 
+const torusMaterial = new THREE.MeshNormalMaterial(1)
+const testTorus = new THREE.Mesh(torusKnotGeometry, torusMaterial)
+
 scene.add(testSphere)
+scene.add(testTorus)
 
 /*******************
 ** ANIMATION LOOP **
@@ -49,6 +54,11 @@ const animation = () =>
 
     // Animate testSphere
     testSphere.position.y = Math.sin(elapsedTime)
+    testTorus.position.y = Math.sin(elapsedTime)
+
+    testTorus.rotation.x = Math.sin(elapsedTime * 2)
+    testTorus.rotation.y = Math.cos(elapsedTime / 2)
+    testTorus.rotation.z = Math.tan(elapsedTime / 10)
     
     // Renderer
     renderer.render(scene, camera)
